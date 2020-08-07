9a121a4fae73903c111429b2f1279374ae858a91
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-08-07 18:41:38 -0500

    mongodb

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(require("./routes/api.js"));
 mongoose.Promise = global.Promise;
 
 mongoose.connect (
-  process.env.MONGODB_URI || "mongodb://User2:password1@ds027489.mlab.com:27489/heroku_wrrcc7n1",
+  process.env.MONGODB_URI || "mongodb://User2:password1@ds237717.mlab.com:37717/heroku_lz2th0lz",
   {
     useMongoClient: true
   }
