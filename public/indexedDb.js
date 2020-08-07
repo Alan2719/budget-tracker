e95b0c1f4cfd58eaacce8113e4a3ece6c87a0b95
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-08-07 18:16:47 -0500

    App working

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,59 +1,63 @@
-//Open method to get access to the DB
-const indexedDb = window.indexedDB;
-let name = document.querySelector("#t-name");
-let amount = document.querySelector("#t-amount");
-let addBtn = document.querySelector("#add-btn");
-let subBtn = document.querySelector("#sub-btn");
+let db;
+db = event.target.result;
+// create a new db request for a "budget" database.
+const request = window.indexedDB.open("budget",1);
 
-//updgraded method to know if the db needs to update
+request.onupgradeneeded = function(event) {
+  const budgetStore = db.createObjectStore("transactions",{autoIncrement:true});
+};
 
-if (indexedDb) {
-    let db;
-    const request = indexedDb.open("budgetTracker",1);
+request.onsuccess = function(event) {
+  db = target.result;
 
-    request.onsuccess = () => {
-        db = request.result;
-        console.log('OPEN',db);
-    }
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
 
-    request.onupgradeneeded = event => {
-        const db = event.target.result;
-        console.log("CREATE", db);
-        const transactionStore = db.createObjectStore("transactions",{keyPath:"name"});
-        //transactionStore.createIndex("transactionID","transaction");
-    }
+request.onerror = function(event) {
+  console.log("GOT AN ERROR :(");
+};
 
-    request.onerror = (err) => {
-        console.log("Error", err);
-    }
-
-    const addData = (data) => {
-        db = request.result;
-        const transaction = db.transaction(["transactions"], "readwrite");
-        const transactionStore = transaction.objectStore("transactions");
-        //const transactionID = transactionStore.index("transactionID");
-
-
-        transactionStore.add(data);
-    }
-
-    addBtn.addEventListener('click',function(event){
-        event.preventDefault();
-        let data = {
-            name: name.value,
-            amount: amount.value
-        }
-        console.log(data);
-        addData(data);
-    });
-
-    subBtn.addEventListener('click',function(event){
-        event.preventDefault();
-        let data = {
-            name: name.value,
-            amount: amount.value
-        }
-        console.log(data);
-        addData(data);
-    });
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  // access your pending object store
+  // add record to your store with add method.
+  const transaction = db.transaction(["transactions"],"readwrite");
+  const budgetStore = transaction.objectStore("transactions");
+  budgetStore.add(record);
 }
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  // access your pending object store
+  // get all records from store and set to a variable
+  const transaction = db.transaction(["transactions"],"readwrite");
+  const store = transaction.objectStore("transactions");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["transactions"],"readwrite");
+          // access your pending object store
+          const store = transaction.objectStore("transactions");
+          // clear all items in your store
+          store.clear();
+      });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
